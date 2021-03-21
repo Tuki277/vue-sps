c35b8040fef81bbbc1a7a654d84781c007867092
Tran Hoang Huy <huybeos2707@gmail.com>
2021-03-21 23:54:53 +0700

    add socket

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { router } from './routes'
+import connect from 'socket.io-client'
+
+const socket = connect('http://localhost:3001')
 
 Vue.config.productionTip = false
 
 new Vue({
+  socket,
   router,
   render: h => h(App),
 }).$mount('#app')
